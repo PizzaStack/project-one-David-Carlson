845c316a6f709c3c44a62e59bfd24b0e1e14bf5c
Sylvernale <david.carlson2012@gmail.com>
2019-01-20 21:44:06 -0600

    Added logout servlet

---
+++ src/main/java/com/revature/servlets/LogoutServlet.java
@@ -0,0 +1,39 @@
+package com.revature.servlets;
+
+import javax.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "Logout", urlPatterns = {"/logout"})
+public class LogoutServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Logout: Starting");
+		response.setContentType("text/html");
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("JSESSIONID")) {
+					System.out.println("JSESSIONID = " + cookie.getValue());
+				}
+			}
+		}
+		
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.invalidate();
+			System.out.println("Session invalidated");
+		}
+		response.sendRedirect("/index.html");
+		System.out.println("Logout: Done");
+	}
+}
